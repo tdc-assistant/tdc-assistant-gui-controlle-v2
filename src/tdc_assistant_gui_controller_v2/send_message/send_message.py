7aa156e138d3edfd6bea16b57349f5dc3821d462
Adam Carter <adam.clayton.carter@gmail.com>
2023-11-13 09:50:34 -0500

    Transform text before sending message

--- src/tdc_assistant_gui_controller_v2/send_message/send_message.py
+++ src/tdc_assistant_gui_controller_v2/send_message/send_message.py
@@ -3,7 +3,26 @@ from pywinauto import mouse, keyboard  # type: ignore
 from .types import Message
 from ..types import Coordinate
 
+originals_and_replacements = {
+    "`": "",
+    "\n": "{VK_RETURN}",
+}
+
+
+def _transform_text(text: str):
+    transformed_text = ""
+    for ch in text:
+        if ch in "{}()":
+            transformed_text += "{" + ch + "}"
+        else:
+            transformed_text += ch
+
+    for original, replacement in originals_and_replacements.items():
+        transformed_text = transformed_text.replace(original, replacement)
+
+    return "{VK_SPACE}".join(transformed_text.split()) + "{VK_RETURN}"
+
 
 def send_message(message: Message, public_chat_coords: Coordinate):
     mouse.click(coords=(public_chat_coords["x"], public_chat_coords["y"]))
-    keyboard.send_keys("{VK_SPACE}".join(message["content"].split()) + "{VK_RETURN}")
+    keyboard.send_keys(_transform_text(message["content"]))
