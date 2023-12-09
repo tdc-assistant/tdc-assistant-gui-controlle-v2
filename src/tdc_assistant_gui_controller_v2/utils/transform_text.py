c8815dafb383dbde6daf677e9e49ea3978d0f1ce
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-09 14:29:18 -0500

    Fix transform text before sending to code editor

--- src/tdc_assistant_gui_controller_v2/utils/transform_text.py
+++ src/tdc_assistant_gui_controller_v2/utils/transform_text.py
@@ -1,16 +1,14 @@
-originals_and_replacements_before = {
-    "`": "",
-}
+originals_and_replacements_before = {"`": "", " ": "{VK_SPACE}"}
 
 originals_and_replacements_after = {
-    "\n": "{VK_RETURN}{VK_LSHIFT HOME}{VK_LSHIFT HOME}{BACKSPACE}",
+    "\n": "{VK_RETURN}{HOME}{HOME}{BACKSPACE}{VK_RETURN}"
 }
 
 
 def transform_text(text: str):
     transformed_text = "\n\n// Adam added this:\n"
     for ch in text:
-        if ch in "{}()":
+        if ch in "{}()+_%":
             transformed_text += "{" + ch + "}"
         else:
             transformed_text += ch
@@ -18,9 +16,9 @@ def transform_text(text: str):
     for original, replacement in originals_and_replacements_before.items():
         transformed_text = transformed_text.replace(original, replacement)
 
-    transformed_text = "{VK_SPACE}".join(transformed_text.split()) + "{VK_RETURN}"
-
     for original, replacement in originals_and_replacements_after.items():
         transformed_text = transformed_text.replace(original, replacement)
+
+    transformed_text += "{VK_RETURN}"
 
     return transformed_text
